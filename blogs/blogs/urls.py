07c0d5c68162206a31ef0cc6a2f0ce025dc1388c
rumancha <rumancha12@gmail.com>
2024-05-06 10:10:12 +0545

    added boards topics and use resusable templates

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from boards import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     path(r'', views.home, name='home'),
 ]
