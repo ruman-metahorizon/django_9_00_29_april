d869e595fa1a165290d825bb6dbeeceb9e29f687
rumancha <rumancha12@gmail.com>
2024-05-07 09:56:59 +0545

    Added new topic

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -23,5 +23,6 @@ from boards import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path(r'', views.home, name='home'),
 ]
