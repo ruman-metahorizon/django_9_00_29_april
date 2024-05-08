ce95f29d8f756080732546382fe8892b1f57b0a9
rumancha <rumancha12@gmail.com>
2024-05-08 10:11:52 +0545

    Upto sign up page

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     'boards',
+    'widget_tweaks',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -19,9 +19,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 
 from boards import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path(r'', views.home, name='home'),
