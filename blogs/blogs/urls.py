98bc7607e22c2ad58788a447e1d8be0f4341c0ae
rumancha <rumancha12@gmail.com>
2024-05-09 10:16:32 +0545

    upto login page

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -20,10 +20,13 @@ from django.urls import path, re_path
 
 from boards import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r"^logout/$", auth_views.LogoutView.as_view(http_method_names = ['get', 'post', 'options']), name="logout"),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path(r'', views.home, name='home'),
