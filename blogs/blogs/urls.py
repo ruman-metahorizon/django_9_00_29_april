f25ffcfa5eda16c84aaa58ae9c0affe59ccb0a9b
rumancha <rumancha12@gmail.com>
2024-05-14 10:17:32 +0545

    upto reply topic

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -24,10 +24,53 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    re_path(r'^signup/$', accounts_views.signup, name='signup'),
-    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    re_path(r"^logout/$", auth_views.LogoutView.as_view(http_method_names = ['get', 'post', 'options']), name="logout"),
-    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
-    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    path(r'', views.home, name='home'),
+    re_path(r"^signup/$", accounts_views.signup, name="signup"),
+    re_path(
+        r"^login/$",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    re_path(
+        r"^logout/$",
+        auth_views.LogoutView.as_view(http_method_names=["get", "post", "options"]),
+        name="logout",
+    ),
+    re_path(r"^boards/(?P<pk>\d+)/$", views.board_topics, name="board_topics"),
+    re_path(r"^boards/(?P<pk>\d+)/new/$", views.new_topic, name="new_topic"),
+    path(r"", views.home, name="home"),
+    re_path(
+        r"^reset/$",
+        auth_views.PasswordResetView.as_view(
+            template_name="password_reset.html",
+            email_template_name="password_reset_email.html",
+            subject_template_name="password_reset_subject.txt",
+        ),
+        name="password_reset",
+    ),
+    re_path(
+        r"^reset/done/$",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path('reset/confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), 
+        name="password_reset_confirm"),
+    re_path(
+        r"^reset/complete/$",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+
+    re_path(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+    name='password_change'),
+    re_path(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+    name='password_change_done'),
+
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
 ]
